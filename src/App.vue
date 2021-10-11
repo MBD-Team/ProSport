164cd2dc9435d2aa89a37bfb9ace8c8e1f798605
Dominic.Blankenfeldt <d.blankenfeldt@mbd-team.de>
2021-10-11 11:26:51 +0200

    bootstrap install/ Logo import to assets/ Navbar build

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
+    <Navbar />
   </div>
   <router-view />
 </template>
+<script>
+import { defineComponent } from 'vue';
+import Navbar from '@/components/navbar.vue';
+
+export default defineComponent({
+  components: { Navbar },
+});
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
---
+++ src/components/navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="navbar">
+    <nav class="">
+      <a class="" href="#">
+        <img src="@/assets/PS_Logo.svg" width="60" height="60" class="" alt="PS_Logo" />
+      </a>
+    </nav>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+<style scoped lang="scss">
+#navbar {
+  background-color: red;
+  padding: 5px;
+}
+img {
+  margin-left: 25px;
+}
+</style>
