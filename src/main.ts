3e7b3405f7237015cc5a966ae9f750a52c854ab5
dsalex1 <dsalex1@ymail.com>
2021-10-11 19:16:07 +0200

    using scss bootstrap instead of css dist

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './main.scss';
+
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all.js';
 
