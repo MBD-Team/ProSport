4c25884638b82acb22ae2c0062daa21fe08597dd
Matthes Staacken <m.staacken@mbd-team.de>
2021-10-11 10:41:36 +0200

    Admin typed correctly

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,8 @@
+interface Admin {
+  name: string;
+  pw: string;
+}
+
 export function getLoginData(): any {
   localStorage.getItem;
   return null;
@@ -6,7 +11,7 @@ export function setUser(): void {
   localStorage.setItem('Username', 'User');
 }
 export function loginAdmin(username: string, password: string): true | false {
-  const Admin = JSON.parse(localStorage.getItem('Admin') || '');
+  const Admin = JSON.parse(localStorage.getItem('Admin') || '') as Admin;
   if (Admin) {
     if (username == Admin.name && password == Admin.pw) return true;
   }
