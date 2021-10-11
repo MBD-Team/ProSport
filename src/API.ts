b7356674d7d7edc628959469bc6fd832f21ce3f3
Matthes Staacken <m.staacken@mbd-team.de>
2021-10-11 15:59:04 +0200

    Settings button when admin and route to settings page

--- src/API.ts
+++ src/API.ts
@@ -1,12 +1,23 @@
 interface Admin {
   name: string;
   pw: string;
+  online: boolean;
 }
 
 export function loginAdmin(username: string, password: string): true | false {
   const Admin = JSON.parse(localStorage.getItem('Admin') || '') as Admin;
   if (Admin) {
-    if (username == Admin.name && password == Admin.pw) return true;
+    if (username == Admin.name && password == Admin.pw) {
+      localStorage.setItem('Admin', JSON.stringify({ name: Admin.name, pw: Admin.pw, online: true }));
+      return true;
+    }
+  }
+  return false;
+}
+export function isAdmin(): true | false {
+  const admin = JSON.parse(localStorage.getItem('Admin') || '');
+  if (admin) {
+    return admin.online;
   }
   return false;
 }
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,22 +1,53 @@
 <template>
-  <div id="navbar">
-    <nav class="">
+  <nav class="d-flex">
+    <div>
       <a class="" href="#">
         <img src="@/assets/PS_Logo.svg" width="60" height="60" class="" alt="PS_Logo" />
       </a>
-    </nav>
-  </div>
+    </div>
+    <div class="d-flex align-items-center" v-if="JSON.parse(admin)">
+      <button class="btn btn-outline-dark me-2" @click="settings()">
+        <i class="fas fa-user-cog" style="font-size: 30px"></i>
+      </button>
+    </div>
+  </nav>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { isAdmin } from '@/API';
 
-export default defineComponent({});
+export default defineComponent({
+  mounted() {
+    this.fetchData();
+  },
+  data() {
+    return {
+      admin: false,
+    };
+  },
+  watch: {
+    $route: 'fetchData',
+  },
+  methods: {
+    fetchData() {
+      isAdmin();
+      console.log('test');
+      this.admin = isAdmin();
+      console.log(this.admin);
+    },
+    settings() {
+      this.$router.push('/Settings');
+    },
+  },
+});
 </script>
 <style scoped lang="scss">
-#navbar {
+nav {
   background-color: red;
   padding: 5px;
+  justify-content: space-between;
 }
+
 img {
   margin-left: 25px;
 }
