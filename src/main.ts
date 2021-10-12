5431ab8438bdbc5760aad70a985186c453b3579f
Matthes Staacken <m.staacken@mbd-team.de>
2021-10-12 12:36:37 +0200

    Firebase Auth, AuthRoute guards and current User abstraction

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
 
 import './main.scss';
 
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all.js';
+
+import router from './router';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCo2ze05SBLKVG5xGPdkZUrpe3VACdKosc',
+  authDomain: 'prosport-2e3be.firebaseapp.com',
+  projectId: 'prosport-2e3be',
+  storageBucket: 'prosport-2e3be.appspot.com',
+  messagingSenderId: '103339976983',
+  appId: '1:103339976983:web:e7dbb5d986bfafe81bef52',
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app');
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,9 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
 import Admin from '../views/Admin.vue';
 import Settings from '../views/Settings.vue';
+import { getAuth, User } from '@firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
+import { ref } from 'vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,6 +21,7 @@ const routes: Array<RouteRecordRaw> = [
     path: '/Settings',
     name: 'Settings',
     component: Settings,
+    meta: { requiresAuth: true },
   },
 ];
 
@@ -26,4 +30,29 @@ const router = createRouter({
   routes,
 });
 
+export const currentUser = ref<User | null>(null);
+
+const getCurrentUser = () => {
+  const auth = getAuth();
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      user => {
+        currentUser.value = user;
+        unsubscribe();
+        resolve(user);
+      },
+      reject
+    );
+  });
+};
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  console.log({ currentUser: await getCurrentUser() });
+  if (requiresAuth && !(await getCurrentUser())) {
+    next('Admin');
+  } else {
+    next();
+  }
+});
 export default router;
