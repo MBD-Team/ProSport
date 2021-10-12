28fbe489492b9af966f1e85f6c95ed157c134b5f
tim <t.littschwager@mbd-team.de>
2021-10-12 16:39:42 +0200

    Exercise props

--- src/components/Exercise.vue
+++ src/components/Exercise.vue
@@ -5,7 +5,7 @@
       <div class="modal-content">
         <div class="modal-header" style="flex-direction: column">
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          <h1 class="modal-title" id="exampleModalToggleLabel">{{ name }}</h1>
+          <h1 class="modal-title" id="exampleModalToggleLabel">{{ exercise?.name }}</h1>
         </div>
         <div class="modal-body">
           <div class="row">
@@ -13,7 +13,7 @@
               <iframe
                 width="720px"
                 height="400px"
-                :src="videoURL"
+                :src="exercise?.videoURL"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -22,13 +22,13 @@
             </div>
             <div class="col-6">
               <h5>Beanspruchte Muskeln:</h5>
-              <p>{{ muscles.join() }}</p>
+              <p>{{ exercise?.muscles.join() }}</p>
               <h5>Beschreibung:</h5>
-              <p>{{ description }}</p>
+              <p>{{ exercise?.description }}</p>
             </div>
             <div>
               <h5>Hinweise:</h5>
-              <p>{{ hints }}</p>
+              <p>{{ exercise?.hints }}</p>
             </div>
           </div>
         </div>
@@ -36,23 +36,12 @@
     </div>
   </div>
 </template>
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue';
-
+import { Exercise } from '@/types';
 export default defineComponent({
-  data() {
-    return {
-      id: '0',
-      name: 'Baseball',
-      description:
-        'Baseball ipsum dolor sit amet cellar rubber win hack tossed. Slugging catcher slide bench league, left fielder nubber. Bullpen blue run rotation relief pitcher grass umpire. Forkball bullpen suicide squeeze club bush league field sport. Base cookie triple play blue hot dog relay rake starting pitcher inning.',
-      hints: 'Baseball ipsum dolor sit amet cellar, rubber win hack tossed. Slugging catche, slide bench league, left fielder nubber. Bullpen blue',
-      videoURL: 'https://www.youtube.com/embed/r_MpUP6aKiQ',
-      img: 'https://www.sueddeutsche.de/image/sz.1.4349344/704x396?v=1551360900',
-      difficulty: 'easy',
-      muscles: ['Baseball', 'ipsum', 'dolor'],
-      trainingDevices: ['sit amet', 'cellar rubber', 'win hack'],
-    };
+  props: {
+    exercise: Object,
   },
 });
 </script>
