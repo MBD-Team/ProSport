5fe6d6736f9467d27190348ef45d13e51b1c3517
Dominic.Blankenfeldt <d.blankenfeldt@mbd-team.de>
2021-10-12 09:54:34 +0200

    update Planning.md/ change name UpperCamelCase

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,8 +7,8 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HumanFront from '@/components/HumanInterface/HumanFront.vue';
-import HumanBack from '@/components/HumanInterface/HumanBack.vue';
+import HumanFront from '@/components/humanInterface/HumanFront.vue';
+import HumanBack from '@/components/humanInterface/HumanBack.vue';
 
 export default defineComponent({
   components: { HumanFront, HumanBack },
