7927e0e28a7ea2f5c48e611e5b45548b5c1b85e7
tim <t.littschwager@mbd-team.de>
2021-10-13 11:16:27 +0200

    refactor Exercise type

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,7 @@
+export type CreatedExercise = Omit<Exercise, 'id'>;
+
 export interface Exercise {
   id: string;
-  name: string;
-  description: string;
-  hints: string;
-  videoURL: string;
-  img: string;
-  difficulty: string;
-  primaryMuscles: string[];
-  secondaryMuscles: string[];
-  trainingDevices: string[];
-}
-export interface CreatedExercise {
   name: string;
   description: string;
   hints: string;
