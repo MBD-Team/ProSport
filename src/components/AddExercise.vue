cd0a238966d2048b6bad8d8a8aae8ab60ddf0bd6
Matthes Staacken <m.staacken@mbd-team.de>
2021-10-14 10:07:49 +0200

    cleaned template, log exercises, convert youtube link to embed url and thumbnail

--- src/components/AddExercise.vue
+++ src/components/AddExercise.vue
@@ -1,102 +1,88 @@
 <template>
-  <div class="container d-flex align-items-stretch justify-content-center flex-column mt-4">
-    <div class="card card-default w-75" style="margin-left: 12.5%">
-      <div class="card-header">Add Exercise</div>
-      <div class="card-body">
-        <form @submit.prevent="addExe()" autocomplete="off">
-          <div class="m-4 alert alert-danger text-center" v-if="error">{{ error }}</div>
-          <div class="p-4 row">
-            <label class="col-4" for="name">Name of Exercise:</label>
-            <div class="col-8">
-              <input minlength="3" class="form-control" type="text" placeholder="name" v-model="exerciseName" autocomplete="off" required />
-            </div>
+  <div class="card card-default">
+    <div class="card-header">Add Exercise</div>
+    <div class="card-body p-4">
+      <form @submit.prevent="addExe()" autocomplete="off">
+        <div class="m-4 alert alert-danger text-center" v-if="error">{{ error }}</div>
+        <div class="mb-4 row">
+          <label class="col-4" for="name">Name of Exercise:</label>
+          <div class="col-8">
+            <input class="form-control" type="text" placeholder="name" v-model="name" autocomplete="off" required />
           </div>
-          <div class="p-4 row">
-            <label class="col-4" for="description">Description:</label>
-            <div class="col-8">
-              <input minlength="3" class="form-control" type="text" placeholder="description" v-model="description" autocomplete="off" required />
-            </div>
+        </div>
+        <div class="mb-4 row">
+          <label class="col-4" for="description">Description:</label>
+          <div class="col-8">
+            <textarea class="form-control" placeholder="description" v-model="description" autocomplete="off" required />
           </div>
-          <div class="p-4 row">
-            <label class="col-4" for="hints">Hints:</label>
-            <div class="col-8">
-              <input
-                minlength="3"
-                class="form-control"
-                type="text"
-                placeholder="hints separated with comma"
-                v-model="hints"
-                autocomplete="off"
-                required
-              />
-            </div>
+        </div>
+        <div class="mb-4 row">
+          <label class="col-4" for="hints">Hints:</label>
+          <div class="col-8">
+            <textarea class="form-control" placeholder="hints" v-model="hints" autocomplete="off" required />
           </div>
-          <div class="p-4 row">
-            <label class="col-4" for="videoURL">VideoURL:</label>
-            <div class="col-8">
-              <input minlength="3" class="form-control" type="text" placeholder="url" v-model="videoURL" autocomplete="off" required />
-            </div>
+        </div>
+        <div class="mb-4 row">
+          <label class="col-4" for="videoURL">VideoURL:</label>
+          <div class="col-8">
+            <input minlength="3" class="form-control" type="text" placeholder="url" v-model="videoURL" autocomplete="off" required />
           </div>
-          <div class="p-4 row">
-            <label class="col-4" for="image">Image:</label>
-            <div class="col-8">
-              <input minlength="3" class="form-control" type="text" placeholder="url" v-model="img" autocomplete="off" required />
-            </div>
-          </div>
-          <div class="p-4 row">
-            <label class="col-4" for="difficulty">Difficulty:</label>
-            <div class="col-8">
-              <div class="btn-group">
-                <input type="radio" class="btn-check" name="difficulty" id="easy" autocomplete="off" @change="difficulty = 'easy'" />
-                <label class="btn btn-outline-primary" for="easy">easy</label>
+        </div>
+        <div class="mb-4" v-if="img"><img :src="img" style="width: 180px; height: 100px" /></div>
+        <div class="alert alert-danger" v-if="videoURL && !img">pls gimme youtube url</div>
+        <div class="mb-4 row">
+          <label class="col-4" for="difficulty">Difficulty:</label>
+          <div class="col-8">
+            <div class="btn-group">
+              <input type="radio" class="btn-check" name="difficulty" id="easy" autocomplete="off" @change="difficulty = 'easy'" />
+              <label class="btn btn-outline-primary" for="easy">easy</label>
 
-                <input type="radio" class="btn-check" name="difficulty" id="medium" autocomplete="off" @change="difficulty = 'medium'" />
-                <label class="btn btn-outline-primary" for="medium">medium</label>
+              <input type="radio" class="btn-check" name="difficulty" id="medium" autocomplete="off" @change="difficulty = 'medium'" />
+              <label class="btn btn-outline-primary" for="medium">medium</label>
 
-                <input type="radio" class="btn-check" name="difficulty" id="hard" autocomplete="off" @change="difficulty = 'hard'" />
-                <label class="btn btn-outline-primary" for="hard">hard</label>
-              </div>
+              <input type="radio" class="btn-check" name="difficulty" id="hard" autocomplete="off" @change="difficulty = 'hard'" />
+              <label class="btn btn-outline-primary" for="hard">hard</label>
             </div>
           </div>
-          <div class="p-4 row">
-            <label class="col-4" for="muscles">primary muscles:</label>
-            <div class="col-8">
-              <Multiselect
-                v-model="primaryMuscles"
-                :options="musclesOptions.filter(p => !secondaryMuscles.includes(p))"
-                mode="tags"
-                :closeOnSelect="false"
-                :searchable="true"
-                noResultsText="no muscles found"
-              />
-            </div>
+        </div>
+        <div class="mb-4 row">
+          <label class="col-4" for="muscles">primary muscles:</label>
+          <div class="col-8">
+            <Multiselect
+              v-model="primaryMuscles"
+              :options="musclesOptions.filter(p => !secondaryMuscles.includes(p))"
+              mode="tags"
+              :closeOnSelect="false"
+              :searchable="true"
+              noResultsText="no muscles found"
+            />
           </div>
-          <div class="p-4 row">
-            <label class="col-4" for="muscles">secondary muscles:</label>
-            <div class="col-8">
-              <Multiselect
-                v-model="secondaryMuscles"
-                :options="musclesOptions.filter(s => !primaryMuscles.includes(s))"
-                mode="tags"
-                :closeOnSelect="false"
-                :searchable="true"
-                noResultsText="no muscles found"
-              />
-            </div>
+        </div>
+        <div class="mb-4 row">
+          <label class="col-4" for="muscles">secondary muscles:</label>
+          <div class="col-8">
+            <Multiselect
+              v-model="secondaryMuscles"
+              :options="musclesOptions.filter(s => !primaryMuscles.includes(s))"
+              mode="tags"
+              :closeOnSelect="false"
+              :searchable="true"
+              noResultsText="no muscles found"
+            />
           </div>
-          <div class="p-4 row">
-            <label class="col-4" for="devices">used equipment:</label>
-            <div class="col-8">
-              <select class="form-control" placeholder="equipment" v-model="trainingDevices" autocomplete="off" require>
-                <option v-for="e in 10" :key="e" :value="e"></option>
-              </select>
-            </div>
+        </div>
+        <div class="mb-4 row">
+          <label class="col-4" for="devices">used equipment:</label>
+          <div class="col-8">
+            <select class="form-control" placeholder="equipment" v-model="trainingDevices" autocomplete="off" require>
+              <option v-for="e in 10" :key="e" :value="e"></option>
+            </select>
           </div>
-          <button class="btn btn-primary m-4 col-3" type="submit" v-if="!addingExercise">add Exercise</button>
-          <span v-else class="m-4 spinner-border spinner-border-sm text-primary"></span>
-          <button class="btn btn-primary m-4 col-3" type="button" @click="showExercises()">show Exercises</button>
-        </form>
-      </div>
+        </div>
+        <button class="btn btn-primary col-3" type="submit" v-if="!addingExercise">add Exercise</button>
+        <span v-else class="spinner-border spinner-border-sm text-primary"></span>
+        <button class="btn btn-primary ms-2 col-3" type="button" @click="showExercises()">show Exercises</button>
+      </form>
     </div>
   </div>
 </template>
@@ -112,11 +98,10 @@ export default defineComponent({
       value: null,
       error: '',
       addingExercise: false,
-      exerciseName: '',
+      name: '',
       description: '',
       hints: '',
       videoURL: '',
-      img: '',
       difficulty: '',
       muscles: [],
       trainingDevices: [] as string[],
@@ -131,32 +116,59 @@ export default defineComponent({
       console.log(res);
     },
     addExe() {
-      if (!this.difficulty || !this.primaryMuscles) {
-        this.error = 'no difficulty or primary muscle chosen';
-        return;
+      if (!this.difficulty) return (this.error = 'no difficulty chosen');
+      if (!this.primaryMuscles) return (this.error = 'no primary muscle chosen');
+
+      let videoURL;
+      let img;
+      if (this.videoURL.includes('youtube')) {
+        let videoID = this.videoURL.split('v=')[1].slice(0, 11);
+        videoURL = `https://www.youtube.com/embed/${videoID}`;
+        img = `https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`;
+      } else if (this.videoURL.includes('youtu.be/')) {
+        let videoID = this.videoURL.split('youtu.be/')[1].slice(0, 11);
+        videoURL = `https://www.youtube.com/embed/${videoID}`;
+        img = `https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`;
+      } else {
+        this.error = 'wrong URL type';
+        return; //not supported
       }
-      this.addingExercise = true;
+
       let newExercise = {
-        name: this.exerciseName,
+        name: this.name,
         description: this.description,
         hints: this.hints,
-        videoURL: this.videoURL,
-        img: this.img,
+        videoURL,
+        img,
         difficulty: this.difficulty,
         trainingDevices: this.trainingDevices,
         primaryMuscles: this.primaryMuscles,
         secondaryMuscles: this.secondaryMuscles,
       };
       try {
+        this.addingExercise = true;
         addExercise(newExercise);
       } catch (e) {
         console.error('Error adding document: ', e);
         this.error = "couldn't add exercise";
+      } finally {
+        this.addingExercise = false;
       }
-      this.addingExercise = false;
     },
     test() {
       console.log('test');
+    },
+  },
+  computed: {
+    img(): string | null {
+      if (this.videoURL.includes('youtube')) {
+        let videoID = this.videoURL.split('v=')[1].slice(0, 11);
+        return `https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`;
+      } else if (this.videoURL.includes('youtu.be/')) {
+        let videoID = this.videoURL.split('youtu.be/')[1].slice(0, 11);
+        return `https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`;
+      }
+      return null;
     },
   },
 });
