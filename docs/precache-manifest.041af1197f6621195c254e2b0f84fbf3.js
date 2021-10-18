a366d99df7ecb6eade0cd96eca9b15c33a036bd6
Matthes Staacken <m.staacken@mbd-team.de>
2021-10-18 15:16:45 +0200

    run build

---
+++ docs/precache-manifest.041af1197f6621195c254e2b0f84fbf3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2af47eaeec5f25e8dbd",
+    "url": "/ProSport/css/app.3f6dbbde.css"
+  },
+  {
+    "revision": "c66b6c0209217e7d266e",
+    "url": "/ProSport/css/chunk-vendors.e54a327a.css"
+  },
+  {
+    "revision": "b1eba7fd343ef47e5f24031ee3ac3e2d",
+    "url": "/ProSport/img/PS_Logo_doublebizeps.b1eba7fd.svg"
+  },
+  {
+    "revision": "926198bc87d46046c657b82591e8db15",
+    "url": "/ProSport/index.html"
+  },
+  {
+    "revision": "f2af47eaeec5f25e8dbd",
+    "url": "/ProSport/js/app.8933f2ff.js"
+  },
+  {
+    "revision": "c66b6c0209217e7d266e",
+    "url": "/ProSport/js/chunk-vendors.c75771ed.js"
+  },
+  {
+    "revision": "1fcdf4ac49a4a0fd5696087e87403f74",
+    "url": "/ProSport/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ProSport/robots.txt"
+  }
+]);
