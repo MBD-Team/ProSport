531284064b2ad3c9dad66898781e462e13850231
tim <t.littschwager@mbd-team.de>
2021-11-25 12:16:00 +0100

    add live/test database

--- src/main.ts
+++ src/main.ts
@@ -17,15 +17,30 @@ import { initializeApp } from 'firebase/app';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyDw9tnK8hRsbSG7sfg_8YnwYUqG9Rh_oMg',
-  authDomain: 'prosport-c559e.firebaseapp.com',
-  projectId: 'prosport-c559e',
-  storageBucket: 'prosport-c559e.appspot.com',
-  messagingSenderId: '734931600540',
-  appId: '1:734931600540:web:456f41cbc5a738138e5959',
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+if (process.env.NODE_ENV === 'production') {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDxATvr8y_oSKxYWUATFshWPd1MKY_7Pok',
+    authDomain: 'prosportlive-2986b.firebaseapp.com',
+    projectId: 'prosportlive-2986b',
+    storageBucket: 'prosportlive-2986b.appspot.com',
+    messagingSenderId: '843803455202',
+    appId: '1:843803455202:web:9647466f0cab0e7587f8bb',
+    measurementId: 'G-MV4JGDQS2F',
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+} else {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDw9tnK8hRsbSG7sfg_8YnwYUqG9Rh_oMg',
+    authDomain: 'prosport-c559e.firebaseapp.com',
+    projectId: 'prosport-c559e',
+    storageBucket: 'prosport-c559e.appspot.com',
+    messagingSenderId: '734931600540',
+    appId: '1:734931600540:web:456f41cbc5a738138e5959',
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+}
 
 createApp(App).use(router).mount('#app');
